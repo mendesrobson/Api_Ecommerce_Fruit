4b819e9c9ea0959cef035ebf98a4ad182a1e0601
Robson Mendes <robsonmendes_1@outlook.com>
2025-09-15 22:06:50 -0300

    produtos

--- Fruit.Api/Domain/Product/Product.cs
+++ Fruit.Api/Domain/Product/Product.cs
@@ -14,3 +14,18 @@ public record class Product
     public string ProductImageUrl { get; set; }
     public int UserId { get; set; }
 }
+
+public record class ProductsResponse
+{
+    public int ProductId { get; set; }
+    public string? ProductSku { get; set; }
+    public string? ProductName { get; set; }
+    public decimal ProductPrice { get; set; }
+    public string? ProductShortName { get; set; }
+    public string? ProductDescription { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? DeliveryTimeSpan { get; set; }
+    public int CategoryId { get; set; }
+    public string? ProductImageUrl { get; set; }
+    public string? CategoryName { get; set; } 
+}
---
+++ Fruit.Api/Domain/Response/ApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Fruit.Api.Domain.Response;
+
+public class ApiResponse<T>
+{
+    public string? Message { get; set; }
+    public bool Result { get; set; }
+    public T? Data { get; set; }
+}
